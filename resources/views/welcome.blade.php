009fa213670f510f3b552bd15149466935e79b6c
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-07-05 20:31:27 +0200

    [UPD] npm i, created basic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Template</title>
-    @vite('resources/js/app.js')
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app')
+
+@section('main')
+@endsection
