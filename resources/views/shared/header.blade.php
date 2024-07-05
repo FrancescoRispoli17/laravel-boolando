806c4b179f0977753f32afc93a38a100889085c7
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-07-05 21:58:15 +0200

    [UPD] Products

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-    <div class="container p-3">
+    <div class="container p-3 fs-4 text-light">
         <div class="row align-items-center">
-            <div class="col-5 fs-4 text-light">
+            <div class="col-5 ">
                 <ul class="m-0">
                     <li>Uomo</li>
                     <li class="mx-3">Donna</li>
@@ -11,8 +11,10 @@
             <div class="col-4">
                 @include('shared.logo')
             </div>
-            <div class="col-3">
+            <div class="col-3 text-end">
                 <i class="fa-regular fa-user"></i>
+                <i class="fa-regular fa-heart mx-2"></i>
+                <i class="fa-solid fa-suitcase"></i>
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,4 +5,18 @@
 @endsection
 
 @section('main')
+
+    <div class="container">
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-4">
+                    <img src="{{ $product['frontImage'] }}" alt="{{$product['name']}}" class="w-50">
+                    <small class="m-0 fs-5 d-block">{{ $product['brand'] }}</small>
+                    <p class="fw-bold fs-4 m-0">{{ $product['name'] }}</p>
+                    <p class="text-danger fw-bold">14,99€ <s class="text-dark">{{ $product['price'] }}</s></p>
+                </div>
+            @endforeach
+        </div>
+    </div>
+        
 @endsection
